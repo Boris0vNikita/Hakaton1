29299cf1f89b2379c76380bdba08ed01913792a4
Nikita Borisov <kukish-2015@mail.ru>
2023-09-19 20:03:29 +0500

    Добавьте файлы проекта.

---
+++ Hakaton1/Infrastructure/DbConn.cs
@@ -0,0 +1,31 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hakaton1.Infrastructure
+{
+    public class DbConn
+    {
+        private const string stringConnection = @"Server=176.56.14.200; Port=5432; Database=user7; UID = prb07; PWD=prb07";
+        private static NpgsqlConnection _sqlConnection;
+
+        public static NpgsqlConnection npgSqlConnection
+        {
+            get
+            {
+                if (_sqlConnection == null)
+                {
+                    _sqlConnection = new NpgsqlConnection(stringConnection);
+                }
+                if (_sqlConnection.State != System.Data.ConnectionState.Open)
+                {
+                    _sqlConnection.Open();
+                }
+                return _sqlConnection;
+            }
+        }
+    }
+}
